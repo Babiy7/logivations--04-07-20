208c2d9884cd206ce47531cf71b81196f96314a5
Babiy <olegbabiy77@gmail.com>
2020-08-03 16:19:19 +0300

    Deleted tags.

--- src/components/UI/Navigation/Navigation.jsx
+++ src/components/UI/Navigation/Navigation.jsx
@@ -7,10 +7,7 @@ function Navigation(props) {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">News</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
+        
             <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
                 <ul class="navbar-nav pr-5">
                     <li class="nav-item dropdown mr-5">
@@ -18,11 +15,13 @@ function Navigation(props) {
                         {state}
                         </a>
                         <div class="dropdown-menu mr-5" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#" onClick={() => setState('Comments')}>Comments</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#" onClick={() => setState('Views')}>Views</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#" onClick={() => setState('Latest')}>Latest</a>
+                        { ['Comments', 'Views', 'Latest'].map(item => {
+                          return <>
+                                <a class="dropdown-item" href="#" onClick={() => setState(item)}>{item}</a>
+                                <div class="dropdown-divider"></div>
+                            </>
+                           } 
+                        )}
                         </div>
                     </li>
                 </ul>
