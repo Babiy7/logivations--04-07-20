c17edfc8676758ae41b18060abeebb39cc43d163
Babiy <olegbabiy77@gmail.com>
2020-08-09 21:52:22 +0300

    Added fab button for basic post.

--- src/containers/BasicPost/BasicPost.jsx
+++ src/containers/BasicPost/BasicPost.jsx
@@ -3,10 +3,10 @@ import './BasicPost.css';
 
 import { useParams, useHistory } from 'react-router-dom';
 import withEffect from '../../hoc/withEffect';
-
 import Spinner from '../../components/UI/Spinner/Spinner';
 import Alert from '../../components/UI/Alert/Alert';
 import Collapse from '../../components/Collapse/Collapse';
+import Fab from '../../components/UI/FabButton/FabButton';
 
 function BasicPost(props) {
     const { loading, posts, error, comments } = props;
@@ -15,11 +15,11 @@ function BasicPost(props) {
     let content = null;
 
     const handleClick = () => {
-        history.push('/');
+        history.goBack();
     }
     
     const handleDelete = () => {
-        history.push('/');
+        history.goBack();
         props.delete(id);
     }
 
@@ -35,16 +35,21 @@ function BasicPost(props) {
         const [ post ] = posts.filter(post => post.id === +id);
         const { title, body } = post;
         content = ( 
-            <div className="basic-post card">
-                <div className="basic-post__header card-header">
-                    <button className="basic-post__return" onClick={handleClick} ></button>
-                    <div className="basic-post__title" >{title ? title : null}</div>
-                    <button className="basic-post__delete" onClick={handleDelete} ></button>
+            <div className='basic-post__container'>
+                <div className="basic-post card">
+                    <div className="basic-post__header card-header">
+                        <button className="basic-post__return" onClick={handleClick} ></button>
+                        <div className="basic-post__title" >{title ? title : null}</div>
+                        <button className="basic-post__delete" onClick={handleDelete} ></button>
+                    </div>
+                    <div className="card-body">
+                        <p className="card-text">{body ? body : null}</p>
+                    </div>
+                    <Collapse comments={comments} id={post.id} />
                 </div>
-                <div className="card-body">
-                    <p className="card-text">{body ? body : null}</p>
+                <div className='basic-post__footer'>
+                    <Fab type='edit' />
                 </div>
-                <Collapse comments={comments} id={post.id} />
             </div>
         )
     }
